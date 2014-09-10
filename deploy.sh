df25f483bd44e426f377a994be096b00d0c56a84
Michael McMillan <email@michaelmcmillan.net>
2014-09-10 11:20:06 +0200

    added excercise 3

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh michaedm@login.stud.ntnu.no 'cd public_html/it2805; wget "https://github.com/michaelmcmillan/IT2805/tarball/master"; tar -xvf "master" --strip 1; rm "master"';
+ssh michaedm@login.stud.ntnu.no 'cd public_html/it2805; wget "https://github.com/michaelmcmillan/IT2805/tarball/master"; tar -xvf "master" --strip 1; rm master*';
