94e4cdf68e2551036e28c41ae58b55db582b59d9
Michael McMillan <email@michaelmcmillan.net>
2014-10-02 16:05:13 +0200

    removed test-dir from course page

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
 		<h2>My IT2805 - Web Technologies Course Page</h2>
 		<ul>
 			<?php foreach  (array_filter(glob('*'), 'is_dir') as $dir): ?>
-			<li><a href="<?php print $dir; ?>">Link to Homework #<?php print $dir; ?></a></li>
+				<?php if ($dir !== 'test'): ?>
+					<li><a href="<?php print $dir; ?>">Link to Homework #<?php print $dir; ?></a></li>
+				<?php endif; ?>
 			<?php endforeach; ?>
 		</ul>
 	</body>
