7b04a6d541f308e7aee15ca2d6f9562d17f7b62d
Michael McMillan <email@michaelmcmillan.net>
2014-10-10 16:07:59 +0200

    added test for console.log 1..10

--- test/assignment-8-1.js
+++ test/assignment-8-1.js
@@ -2,10 +2,22 @@
 casper.test.begin('Assignment 8-1: Log to console 1..20 (inclusively)', 1, function suit (test) {
     var server = casper.cli.get("server");
 
-    casper.start(server + '/8/exercise-1/multimedia.html');
+    casper.start(server + '/8/exercise-1/index.html');
 
-    casper.then(function () {
-        test.assertExists('video[poster="bbb-splash.png"]', "poster attribute was found on video-element");
+    var numbers = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15, 16, 17, 18,
+        19, 20
+    ];
+
+    var receivedNumbers = [];
+
+    casper.on('remote.message', function (msg) {
+        receivedNumbers.push(parseInt(msg));
+    });
+
+    casper.then(function() {
+        test.assertEquals(numbers, receivedNumbers, 'console.logged numbers are 1..20');
     });
 
     casper.run(function() {
