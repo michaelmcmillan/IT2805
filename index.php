d65b54a68827fd63f95ed560893459825688acb2
Michael McMillan <email@michaelmcmillan.net>
2014-10-26 19:35:55 +0100

    sort dirs from glob numerically (as each dir represents the assignment)

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
 	<body>
 		<h2>My IT2805 - Web Technologies Course Page</h2>
 		<ul>
-			<?php foreach  (array_filter(glob('*'), 'is_dir') as $dir): ?>
-				<?php if ($dir !== 'test'): ?>
-					<li><a href="<?php print $dir; ?>">Link to Homework #<?php print $dir; ?></a></li>
+			<?php
+			$assignments = array_filter(glob('*'), 'is_dir');
+			sort($assignments, SORT_NUMERIC);
+			foreach ($assignments as $dir => $assignment): ?>
+				<?php if ($assignment !== 'test'): ?>
+					<li><a href="<?php print $dir; ?>">Link to Homework #<?php print $assignment; ?></a></li>
 				<?php endif; ?>
 			<?php endforeach; ?>
 		</ul>
